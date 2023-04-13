6bbd1577816f45dffe99b8b41c7b81cd0afd72b8
Edris <edris.dosky@gmail.com>
2023-04-13 18:34:10 +0300

    create file for user and verify user to login

---
+++ includes/user.php
@@ -0,0 +1,23 @@
+<?php
+
+use User as GlobalUser;
+
+class User extends Api{
+    public $user_id ;
+    public $username;
+    public $tell;
+    public $email;
+    public $password;
+    public $photo;
+
+    protected static $columns =  array('user_id','username','tell','email','password','photo');
+
+    public static function verify($username , $password){
+        $password = hash('gost' , $password);
+        $single_user_data = self::query_proccess("SELECT * FROM `user` WHERE `username` ='$username' AND `password` = '$password' LIMIT 1");
+        return !empty($single_user_data) ? array_shift($single_user_data) : false;        
+    }
+
+}
+$user = new User();
+?>
