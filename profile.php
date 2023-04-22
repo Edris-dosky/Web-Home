1c0e0df71f7dc1c3a3f01091484b173f4c7509c3
Edris <edris.dosky@gmail.com>
2023-04-22 23:36:33 +0300

    get data in database and enter in profile page

--- profile.php
+++ profile.php
@@ -5,7 +5,64 @@ if(isset($_GET['logout'])){
     header("location:Web-Home\home.php");
 }
 ?>
-
-<a href="?logout" class="btn btn-danger w-100">LOGOUT</a>
+<body class="font-display bg-zinc-100 w-full">
+    <div class=" container mx-auto p-16">
+        <div class="p-8 bg-white shadow mt-24">
+          <div class="grid grid-cols-1 md:grid-cols-3">
+            <div class="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0">
+              <div>
+                <p class="font-bold text-gray-700 text-xl">22</p>
+                <p class="text-gray-400">خانوو بۆ فرۆشتن</p>
+              </div>
+              <div>
+                   <p class="font-bold text-gray-700 text-xl">10</p>
+                <p class="text-gray-400">خانوو بۆکرێ</p>
+              </div>
+              <div>
+                <p class="font-bold text-gray-700 text-xl">0</p>
+             <p class="text-gray-400">ئەرز</p>
+           </div>
+            </div>
+            <div class="relative">
+              <div class="w-52 h-52 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 -top-4 -mt-24 flex items-center justify-center text-[#188F8D]">
+        <svg xmlns="http://www.w3.org/2000/sv" class="h-24 w-24" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+        </svg>
+              </div>
+            </div>
+        
+            <div class="space-x-8 flex justify-between mt-32 md:mt-0 md:justify-center">
+        <a href=""
+          class="text-white py-3 px-4 uppercase rounded bg-[#188F8D] hover:bg-green-600 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
+        >
+          خانووەکان
+        </a>
+            <a href=""
+          class="text-white pt-3 px-4 uppercase rounded bg-gray-700 hover:bg-gray-800 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
+        >
+          نامەناردن
+        </a>
+            </div>
+          </div>
+        
+          <div class="mt-20 text-center border-b pb-12">
+            <h1 class="text-4xl font-medium text-gray-700"><?php echo $obj->username; ?> <span class="font-light text-gray-500"></span></h1>
+            <p class="font-light text-gray-600 mt-3">iraq - erbil</p>
+        
+            <p class="mt-8 text-gray-500"><?php echo $obj->tell; ?></p>
+            <p class="mt-2 text-gray-500"><?php echo $obj->email; ?></p>
+          </div>
+        
+          <div class="mt-12 flex flex-col justify-center">
+            <p class="text-gray-600 text-center font-light lg:px-16">هەر هەژمارێک خۆی بەرپرسیارە لەوەی دایدەنێ، وە دەتوانێ هەرچەند بیەوێ خانوو و ئەرزی خۆی دابنێ و دانان بێ بەرامبەرە </p>
+            <button class="text-indigo-500 py-2 px-4  font-medium mt-4">
+              .... بینینی زیاتر
+            </button>
+            <a href="?logout" class="btn btn-danger w-100">LOGOUT</a>
+          </div>
+        
+        </div>
+        </div>
+       
 </body>
 </html>
