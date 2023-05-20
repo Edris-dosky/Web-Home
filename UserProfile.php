18d67440d07d49b34db3337a15afa864d837d799
Edris <edris.dosky@gmail.com>
2023-05-20 22:58:30 +0300

    create new file to see user posts

---
+++ UserProfile.php
@@ -0,0 +1,85 @@
+<?php
+require_once('includes/nav.php');
+
+function get_row($col){
+  global $userid ;
+  $result = Post::get_all("WHERE '$userid' = `user_id` AND '$col' = `type`");
+  echo count($result);
+}
+
+if($_GET['ID']){
+  $userid = $_GET['ID'] ;
+  $UserData = User::get_one("`user_id` = '$userid' "); 
+}else{
+  go("home.php");
+}
+?>
+<body class="font-display bg-zinc-100 w-full">
+    <div class=" container mx-auto p-16">
+        <div class="p-8 bg-white shadow mt-24">
+          <div class="grid grid-cols-1 md:grid-cols-3">
+            <div class="grid grid-cols-4 text-center order-last md:order-first mt-20 md:mt-0">
+              <div>
+                <p class="font-bold text-gray-700 text-xl"><?php get_row("خانوو") ?></p>
+                <p class="text-gray-400">خانوو</p>
+              </div>
+              <div>
+                   <p class="font-bold text-gray-700 text-xl"><?php get_row("شوقە") ?></p>
+                <p class="text-gray-400">شوقە </p>
+              </div>
+              <div>
+                <p class="font-bold text-gray-700 text-xl"><?php get_row("ڤێلا") ?></p>
+             <p class="text-gray-400">ڤێلا</p>
+           </div>
+           <div>
+                <p class="font-bold text-gray-700 text-xl"><?php get_row("ئەرز") ?></p>
+             <p class="text-gray-400">ئەرز</p>
+           </div>
+            </div>
+            <div class="relative">
+              <div class="w-52 h-52 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 -top-4 -mt-24 flex items-center justify-center text-[#188F8D]">
+              <?php if($UserData->photo){ ?>
+             <img class="w-full h-full rounded-full object-cover" src="./upload/<?php echo $UserData->photo ?>"  alt="user photo"> 
+		      	 <?php }else{ ?>
+              <svg xmlns="http://www.w3.org/2000/sv" class="h-24 w-24" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+                 </svg>
+                 <?php } ?>
+              </div>
+            </div>
+        
+            <div class="space-x-8 flex justify-between mt-32 md:mt-0 md:justify-center">
+        <a href=""
+          class="text-white py-3 px-4 uppercase rounded bg-[#188F8D] hover:bg-green-600 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
+        >
+          موڵکەکانی
+        </a>
+            <a href=""
+          class="text-white pt-3 px-4 uppercase rounded bg-gray-700 hover:bg-gray-800 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
+        >
+          نامەناردن
+        </a>
+            </div>
+          </div>
+        
+          <div class="mt-20 text-center border-b pb-12">
+            <h1 class="text-4xl font-medium text-gray-700"><?php echo $obj->username; ?> <span class="font-light text-gray-500"></span></h1>
+            <p class="font-light text-gray-600 mt-3">iraq - erbil</p>
+        
+            <p class="mt-8 text-gray-500"><?php echo $UserData->tell; ?></p>
+            <p class="mt-2 text-gray-500"><?php echo $UserData->email; ?></p>
+          </div>
+        
+          <div class="mt-12 flex flex-col justify-center">
+            <p class="text-gray-600 text-center font-light lg:px-16">هەر هەژمارێک خۆی بەرپرسیارە لەوەی دایدەنێ، وە دەتوانێ هەرچەند بیەوێ خانوو و ئەرزی خۆی دابنێ و دانان بێ بەرامبەرە </p>
+            <button class="text-indigo-500 py-2 px-4  font-medium mt-4">
+              .... بینینی زیاتر
+            </button>
+            
+          </div>
+          <a href="?logout" class="text-white p-3 px-4 uppercase rounded bg-red-400 hover:bg-red-600 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">چونەدەرەوە</a>
+        </div>
+        </div>
+       
+</body>
+</html>
--- includes/nav.php
+++ includes/nav.php
@@ -47,13 +47,13 @@ $obj= User::get_one("`tell`='$tell'");
 					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
 				</svg>
 			</li>
-			<li><a class="text-lg text-gray-500 hover:text-[#188F8D] hover:font-bold " href="myPost.php">موڵکەکانم</a></li>
+			<li><a class="text-lg text-gray-500 hover:text-[#188F8D] hover:font-bold " href="Posts.php">موڵکەکانم</a></li>
 			<li class="text-gray-300">
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" class="w-4 h-4 current-fill" viewBox="0 0 24 24">
 					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
 				</svg>
 			</li>
-			<li><a class="text-lg text-gray-500 hover:text-[#188F8D] hover:font-bold " href="">دەربارەی ئێمە</a></li>
+			<li><a class="text-lg text-gray-500 hover:text-[#188F8D] hover:font-bold " href="UserProfile.php?ID=1">دەربارەی ئێمە</a></li>
 		</ul>
 		<?php if(!$session->get_logged_in()){?>
 		<a class="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-100 hover:bg-gray-200 text-sm text-gray-900 font-bold  rounded-xl transition duration-200" href="login.php">چونەژوەوە</a>
@@ -111,7 +111,7 @@ $obj= User::get_one("`tell`='$tell'");
 						<a class="text-right block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-[#188F8D] rounded" href="favPost.php">دڵخوازەکانم</a>
 					</li>
 					<li class="mb-1">
-						<a class="text-right block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-[#188F8D] rounded" href="myPost.php">موڵکەکانم</a>
+						<a class="text-right block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-[#188F8D] rounded" href="Posts.php">موڵکەکانم</a>
 					</li>
 					<li class="mb-1">
 						<a class="text-right block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-[#188F8D] rounded" href="#">دەربارەی ئێمە</a>
